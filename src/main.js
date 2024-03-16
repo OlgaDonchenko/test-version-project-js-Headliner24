c080ec563ce8b4cf0bc5065af5bef9d38e6d4d7d
OlgaDonchenko <n0931606045@gmail.com>
2024-03-17 00:35:31 +0200

    project section

---
+++ src/main.js
@@ -0,0 +1,27 @@
+
+import Swiper from 'swiper';
+const nextButton = document.querySelector('.project-swiper-button-next');
+const prevButton = document.querySelector('.project-swiper-button-prev');
+
+nextButton.addEventListener('click', () => {
+  swiper.slideNext();
+});
+
+prevButton.addEventListener('click', () => {
+  swiper.slidePrev();
+});
+
+
+const swiper = new Swiper('.project-swiper-container', {
+  slidesPerView: 1, 
+  spaceBetween: 30,
+  pagination: {
+    el: '.project-swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.project-swiper-button-left',
+    prevEl: '.project-swiper-button-right',
+  },
+});
+console.log();
