51c8120c1b8030a891a9b92b9097b05e0602b803
OlgaDonchenko <n0931606045@gmail.com>
2024-03-17 12:22:25 +0200

    htthth

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
 
-import Swiper from 'swiper';
-const nextButton = document.querySelector('.project-swiper-button-next');
-const prevButton = document.querySelector('.project-swiper-button-prev');
+// import Swiper from 'swiper';
+// const nextButton = document.querySelector('.project-swiper-button-next');
+// const prevButton = document.querySelector('.project-swiper-button-prev');
 
-nextButton.addEventListener('click', () => {
-  swiper.slideNext();
-});
+// nextButton.addEventListener('click', () => {
+//   swiper.slideNext();
+// });
 
-prevButton.addEventListener('click', () => {
-  swiper.slidePrev();
-});
+// prevButton.addEventListener('click', () => {
+//   swiper.slidePrev();
+// });
 
 
-const swiper = new Swiper('.project-swiper-container', {
-  slidesPerView: 1, 
-  spaceBetween: 30,
-  pagination: {
-    el: '.project-swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.project-swiper-button-left',
-    prevEl: '.project-swiper-button-right',
-  },
-});
-console.log();
+// const swiper = new Swiper('.project-swiper-container', {
+//   slidesPerView: 1, 
+//   spaceBetween: 30,
+//   pagination: {
+//     el: '.project-swiper-pagination',
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: '.project-swiper-button-left',
+//     prevEl: '.project-swiper-button-right',
+//   },
+// });
+// console.log();
